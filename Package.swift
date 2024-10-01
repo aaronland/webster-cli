4fa7d1fa891afafa643c3636f6d78ea73c8a3aab
thisisaaronland <thisisaaronland@localhost>
2024-10-01 09:10:20 -0700

    swift6...

--- Package.swift
+++ Package.swift
@@ -1,23 +1,27 @@
-// swift-tools-version:5.2
+// swift-tools-version:6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "webster-cli",
+    platforms: [
+        .macOS(.v10_15)
+    ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "0.3.2"),
-        .package(url: "https://github.com/aaronland/swift-webster", from: "0.0.3")
-        // .package(url: "file:///Users/asc/code/swift-webster", .branch("dev"))
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.5.0"),
+        //.package(url: "https://github.com/aaronland/swift-webster", from: "0.0.3")
+        .package(path: "/Users/asc/code/swift-webster")
 
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
+        .executableTarget(
             name: "webster-cli",
+            
             dependencies: [
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
                 .product(name: "Webster", package: "swift-webster"),
@@ -27,7 +31,3 @@ let package = Package(
             dependencies: ["webster-cli"]),
     ]
 )
-
-/*
- /Users/asc/code/webster-cli/Package.swift: dependency 'Webster' in target 'webster-cli' requires explicit declaration; provide the name of the package dependency with '.package(name: "Webster", url: "https://github.com/aaronland/swift-webster", .branch("master"))'
- */
